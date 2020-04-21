bdde933f1245b466e35cf1716f366762ffd9fd7c
Saurabh Prakash <saurabhprakash.smvit@gmail.com>
2020-04-22 01:22:31 +0530

    Do refactoring

--- C++/1 - Introduction/2 - Input and Output.cpp
+++ C++/1 - Introduction/2 - Input and Output.cpp
@@ -9,11 +9,11 @@ int main ()
         int num;
         int sum = 0;
 
-    while (std::cin>>num)
+    while (std::cin >> num)
     {
         sum += num;
     }
 
-    std::cout<<sum<<std::endl;
+    std::cout << sum << std::endl;
     return 0;
 }
