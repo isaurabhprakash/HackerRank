3a6406e6177d3ec60f770381d85111be6c43f47c
Saurabh Prakash <saurabhprakash.smvit@gmail.com>
2020-04-23 18:54:47 +0530

    Create 4 - Conditional Statements.cpp

---
+++ C++/1 - Introduction/4 - Conditional Statements.cpp
@@ -0,0 +1,21 @@
+// URL : https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem
+
+#include <iostream>
+#include <string>
+
+int main ()
+{
+        int n;
+        std::string number[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    if(std::cin>>n)
+    {
+        if (n > 9) {
+
+            std::cout<<"Greater than 9"<<std::endl;
+
+        } else {
+            std::cout<<number[n-1]<<std::endl;
+        }
+    }
+    return 0;
+}
