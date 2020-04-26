4415bf4c450b4486be1340fa5cf13150cbe0a29b
Saurabh Prakash <saurabhprakash.smvit@gmail.com>
2020-04-26 21:53:49 +0530

    Create 1 - Vector-Sort.cpp

---
+++ C++/4 - STL/1 - Vector-Sort.cpp
@@ -0,0 +1,36 @@
+// URL : https://www.hackerrank.com/challenges/vector-sort/problem
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main ()
+{
+        std::vector<int>    vector_number;
+        int                 number;
+        int                 no_of_elements;
+
+    std::cin>>no_of_elements;
+
+    while (no_of_elements--)
+    {
+        if (std::cin>>number) {
+            
+            vector_number.push_back (number);
+        }
+    }
+
+
+    // Sort the elements of the vector.
+    sort (vector_number.begin(), vector_number.end());
+
+    // Create an iterator for the vector and use it
+    // Also doint pre-increment instead of post-increment would require less processing and memory
+    // as a temporary variable would not be needed in this case.
+    // Need to verify this from the assembly code.
+    for (std::vector<int>::iterator it = vector_number.begin(); it!= vector_number.end(); ++it)
+    {
+        std::cout<<*it<<" ";
+    }
+    return 0;
+}
