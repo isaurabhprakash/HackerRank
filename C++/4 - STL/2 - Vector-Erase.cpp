942c2a78091a42d94c3f65848856a8b5ce75d07e
Saurabh Prakash <saurabhprakash.smvit@gmail.com>
2020-05-03 16:22:35 +0530

    Create 2 - Vector-Erase.cpp

---
+++ C++/4 - STL/2 - Vector-Erase.cpp
@@ -0,0 +1,40 @@
+// URL : https://www.hackerrank.com/challenges/vector-erase/problem
+#include <iostream>
+#include <vector>
+
+int main ()
+{
+        int                 temp; 
+        int                 temp1;;
+        std::vector<int>    v;
+    
+    std::cin>>temp;
+
+    while (temp--) {         
+
+        std::cin>>temp1;
+        v.push_back(temp1);
+    }
+
+    std::cin>>temp;
+
+    // Since the vecotr index starts with 1 here.
+    temp = temp - 1;
+    v.erase(v.begin() + temp);
+
+    std::cin>>temp>>temp1;
+
+    // Since the vecotr index starts with 1 here.
+    temp = temp -1;
+    temp1 = temp1 - 1;
+
+    v.erase(v.begin() + temp, v.begin() + temp1);
+
+    std::cout<<v.size ()<<std::endl;
+
+    for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+
+        std::cout<<*it<<" ";
+    }
+    return 0;
+}
