73c9777228bce5c164c59ca9861fb65c49676f7f
Saurabh Prakash <saurabhprakash.smvit@gmail.com>
2020-05-14 12:27:15 +0530

    Create 4 - Sets-STL.cpp

---
+++ C++/4 - STL/4 - Sets-STL.cpp
@@ -0,0 +1,52 @@
+// URL : https://www.hackerrank.com/challenges/cpp-sets/problem
+
+#include <iostream>
+#include <set>
+
+#define ADD     1
+#define DELETE  2
+#define FIND    3
+
+int main ()
+{
+        int             no_of_queries;
+        int             action;
+        int             element;
+        std::set<int>   s;
+
+        
+    
+    std::cin>>no_of_queries;
+
+    while (no_of_queries--)
+    {
+        std::cin>>action;
+        std::cin>>element;
+
+        switch (action) 
+        {
+            case ADD : 
+
+                s.insert(element);
+                break;
+
+            case DELETE :
+
+                s.erase (element);
+                break;
+
+            case FIND :
+
+                if (s.find(element) != s.end())
+                {
+                    std:: cout<<"Yes"<<std::endl;
+                } else {
+
+                    std:: cout<<"No"<<std::endl;
+                }
+        }
+
+    }
+
+    return 0;   
+}
